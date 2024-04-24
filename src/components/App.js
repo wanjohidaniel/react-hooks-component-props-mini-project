c6b41040b2a5a23ab9b0116cfb010f79781df66e
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-04-24 21:10:24 +0300

    Components and Props

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-import React from "react";
-import blogData from "../data/blog";
-
-console.log(blogData);
+import React from 'react';
+import Header from './Header';
+import About from './About';
+import ArticleList from './ArticleList';
 
 function App() {
+  const posts = [
+    {
+      id: 1,
+      title: "Components 101",
+      date: "December 15, 2020",
+      preview: "Setting up the building blocks of your site",
+    },
+    {
+      id: 2,
+      title: "React Data Flow",
+      date: "December 11, 2020",
+      preview: "Passing props is never passé",
+    },
+    {
+      id: 3,
+      title: "Function Components vs Class Components",
+      date: "December 10, 2020",
+      preview: "React, meet OOJS.",
+    },
+  ];
+
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name="Underreacted" />
+      <About />
+      <ArticleList posts={posts} />
     </div>
   );
 }
---
+++ src/components/ArticleList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Article from './Article';
+
+const ArticleList = ({ posts }) => {
+  return (
+    <main>
+      {posts.map((post) => (
+        <Article key={post.id} post={post} />
+      ))}
+    </main>
+  );
+};
+
+export default ArticleList;
